cc7ba26d55e2c34f66aa788bd75f0ee35399411c
icamdaf <icampoltek@gmail.com>
2024-05-29 14:37:56 +0700

    pertemuan 07

---
+++ MahasiswaMain20.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class MahasiswaMain20 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Scanner sl = new Scanner(System.in);
+        int jumMhs;
+        
+        System.out.println("---------------------------------------------------------");
+        System.out.print("Masukkan berapa banyak data yang ingin ditambahkan : ");
+        jumMhs = s.nextInt();
+        
+        PencarianMhs20 data = new PencarianMhs20(jumMhs);
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Masukkan data mahasiswa secara Urut dari Nim Terkecil");
+
+        for (int i = 0; i < jumMhs; i++) {
+            System.out.println("---------------------------------------------------------");
+            System.out.print("Nim\t: ");
+            int nim = s.nextInt();
+            System.out.print("Nama\t : ");
+            String nama = sl.nextLine();
+            System.out.print("Umur\t : ");
+            int umur = s.nextInt();
+            System.out.print("IPK\t : ");
+            double ipk = s.nextDouble();
+
+            Mahasiswa20 m = new Mahasiswa20(nim, nama, umur, ipk);
+            data.tambah(m);
+        }
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Data keseluruhan Mahasiswa : ");
+        data.tampil();
+
+         System.out.println("---------------------------------------------------------");
+         System.out.println("---------------------------------------------------------");
+         System.out.println("Pencarian Data : ");
+         System.out.println("Masukkan Nim Mahasiswa yang dicari: ");
+         System.out.print("NIM : ");
+         int cari = s.nextInt();
+         System.out.println("menggunakan sequential Search");
+         int posisi = data.FindSeqSearch(cari);
+
+         data.Tampilposisi(cari, posisi);
+         data.TampilData(cari, posisi);
+
+
+
+      
+
+        System.out.println("---------------------------------------------------------");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Pencarian Data : ");
+        System.out.println("Masukkan Nama Mahasiswa yang dicari: ");
+        System.out.print("Nama : ");
+        String cari = s.next();
+
+        System.out.println("==========================================================");
+        System. out.println("menggunakan binary Search") ;
+
+
+         posisi = data.FindBinarySearch(cari, 0, jumMhs - 1);
+        data.countSearchName(cari);
+        int posisi = data.FindBinarySearchByName(cari, 0, jumMhs - 1, 0);
+        data.TampilposisiNama(cari, posisi);
+        data.TampilDataNama (cari, posisi);
+
+    }
+}
---
+++ PencarianMhs20.java
@@ -0,0 +1,120 @@
+public class PencarianMhs20 {
+    Mahasiswa20 listMHs[];
+    int idx;
+    public PencarianMhs20(int jumMhs) {
+        listMHs = new Mahasiswa20[jumMhs]; 
+    }
+
+    void tambah(Mahasiswa20 m) {
+        if (idx < listMHs.length) {
+            listMHs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh !! ");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa20 m : listMHs) {
+            m.tampil();
+            System.out.println("-------------------------");
+        }
+    }
+
+    public int FindSeqSearch(int cari) {
+        int posisi = -1;
+        for (int j = 0; j < listMHs.length; j++) {
+            if (listMHs[j].nim == cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    public void Tampilposisi(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("data : " + x + " ditemukan pada indeks " + pos);
+        } else {
+            System.out.println("data " + x + " tidak ditemukan");
+        }
+    }
+
+    public void TampilData(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("Nim\t : " + x);
+            System.out.println("Nama\t : " + listMHs[pos].nama);
+            System.out.println("Umur\t : " + listMHs[pos].umur);
+            System.out.println("IPK\t : " + listMHs[pos].ipk);
+        } else {
+            System.out.println("data " + x + " tidak ditemukan");
+        }
+    }
+    public void TampilposisiNama(String x, int pos) {
+        if (pos != -1) {
+            System.out.println("data : " + x + " ditemukan pada indeks " + pos);
+        } else {
+            System.out.println("data " + x + " tidak ditemukan");
+        }
+    }
+
+    public void TampilDataNama(String x, int pos) {
+        if (pos != -1) {
+            System.out.println("Nim\t : " + x);
+            System.out.println("Nama\t : " + listMHs[pos].nama);
+            System.out.println("Umur\t : " + listMHs[pos].umur);
+            System.out.println("IPK\t : " + listMHs[pos].ipk);
+        } else {
+            System.out.println("data " + x + " tidak ditemukan");
+        }
+    }
+
+     public int FindBinarySearch(int cari, int left, int right) {
+         int mid;
+         if (right >= left) {
+             mid = (left + right) / 2;
+           
+             if (listMHs[mid].nim < cari) {
+                 return FindBinarySearch(cari, left, mid - 1);
+             } else if (listMHs[mid].nim > cari) {
+                 return FindBinarySearch(cari, mid + 1, right);
+             } else {
+                 return mid;
+             }
+         }
+
+         return -1;
+     }
+
+    public int FindBinarySearchByName(String cari, int left, int right, int count) {
+        if (right >= left) {
+
+            int mid = (left + right) / 2;
+            int comparison = listMHs[mid].nama.compareTo(cari);
+
+            if (comparison < 0) {
+                return FindBinarySearchByName(cari, mid + 1, right, count);
+            } else if (comparison > 0) {
+                return FindBinarySearchByName(cari, left, mid - 1, count);
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public void countSearchName(String cari)
+    {
+        int count = 0;
+        for(int i=0; i<listMHs.length; i++){
+            if(cari.equalsIgnoreCase(listMHs[i].nama)){
+                count += 1;
+            }
+        }
+
+        if (count > 1) {
+            System.out.println("Data yang ditemukan lebih dari 1");
+            return;
+        }
+    }
+}
